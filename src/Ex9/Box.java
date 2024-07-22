017ac936b17e101627a5c9683f3e6554728e93d6
Tihon-k <adidas21-00@mail.ru>
2024-07-22 20:28:32 +0300

    Ex9,10

--- src/Ex9/Box.java
+++ src/Ex9/Box.java
@@ -7,14 +7,11 @@ public class Box extends Shape {
         this.volume = boxSize;
     }
 
-    Ball ball = new Ball(1);
-    Cylinder cylinder = new Cylinder(1,1);
-    Pyramid pyramid = new Pyramid(1,1);
 
 
     public boolean add(Shape shape) {
-        double n  = volume - (ball.volume + cylinder.volume + pyramid.volume);
-        if (n >= 0) {
+        this.volume  = this.volume - shape.getVolume();
+        if (this.volume >= 0) {
             return true;
         } else return false;
     }
