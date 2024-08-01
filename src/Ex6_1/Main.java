9a776a33fafc2296c04ca1304fa63b71da01edc4
Tihon-k <adidas21-00@mail.ru>
2024-08-01 23:10:42 +0300

    Site- 6.2 Filter v2 (without arraycopy)

--- src/Ex6_1/Main.java
+++ src/Ex6_1/Main.java
@@ -5,27 +5,32 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        Object[] ob = new Object[2];
+        ob[1] = 1;
+        IsNullFilter isNullFilter = new IsNullFilter();
+
+        filter(ob, isNullFilter);
     }
 
 
-
     public static Object[] filter(Object[] objects, FilterInterface filterInterface) {
-
         List list = new ArrayList();
         for (int i = 0; i < objects.length; i++) {//прохождение по всему массиву
             Object element = objects[i];
 
             boolean answer = filterInterface.apply(element);
 
-            if(answer == false){
+            if (answer == false) {
                 list.add(element);
             }
         }
-        Object[] objects1 = new Object[list.size()];
-        System.arraycopy(list, 0,objects1,0,list.size());
-        return objects1;
+
+        Object[] obj = new Object[list.size()];
+
+        for (int n = 0; n < list.size(); n++) {
+            obj[n] = list.get(n);
+        }
+        return obj;
 
     }
-
-
 }
